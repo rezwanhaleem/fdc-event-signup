3874aea7a0c3905b2bf03d1e5fd69ffb5d74e27c
Rezwan Azfar Haleem <rezwan.haleem@gmail.com>
2022-01-12 01:19:44 -0500

    Workflow build directories set

--- server/src/test.js
+++ server/src/test.js
@@ -1,7 +1,7 @@
 const { app } = require('./index');
 const request = require('supertest');
 
-test('test ping', () => {
+test('test ping', async () => {
     const res = await request(app).get('/');
     expect(res.status).toBe(200);
     expect(res.body).toEqual('pong');
